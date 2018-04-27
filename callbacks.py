7801873a9ee57c4d473504f5d8affc7704cce542
Dmytro S Lituiev <d.lituiev@gmail.com>
2018-04-26 17:24:17 -0700

    CSVWallClockLogger

--- callbacks.py
+++ callbacks.py
@@ -1220,3 +1220,90 @@ class LambdaCallback(Callback):
             self.on_train_end = on_train_end
         else:
             self.on_train_end = lambda logs: None
+
+class CSVWallClockLogger(Callback):
+    """Callback that streams epoch results to a csv file.
+    Supports all values that can be represented as a string,
+    including 1D iterables such as np.ndarray.
+    # Example
+        ```python
+        csv_logger = CSVLogger('training.log')
+        model.fit(X_train, Y_train, callbacks=[csv_logger])
+        ```
+    # Arguments
+        filename: filename of the csv file, e.g. 'run/log.csv'.
+        separator: string used to separate elements in the csv file.
+        append: True: append if file exists (useful for continuing
+            training). False: overwrite existing file,
+    """
+
+    def __init__(self, filename, separator=',', append=False, compute_auc=False):
+        self.compute_auc = compute_auc
+        self._prev_time_ = time.time()
+        self.sep = separator
+        self.filename = filename
+        self.append = append
+        self.writer = None
+        self.keys = None
+        self.append_header = True
+        self.file_flags = 'b' if six.PY2 and os.name == 'nt' else ''
+        super(CSVWallClockLogger, self).__init__()
+
+    def on_train_begin(self, logs=None):
+        if self.append:
+            if os.path.exists(self.filename):
+                with open(self.filename, 'r' + self.file_flags) as f:
+                    self.append_header = not bool(len(f.readline()))
+            self.csv_file = open(self.filename, 'a' + self.file_flags)
+        else:
+            self.csv_file = open(self.filename, 'w' + self.file_flags)
+
+    def on_epoch_end(self, epoch, logs=None):
+        logs = logs or {}
+
+        def handle_value(k):
+            is_zero_dim_ndarray = isinstance(k, np.ndarray) and k.ndim == 0
+            if isinstance(k, six.string_types):
+                return k
+            elif isinstance(k, Iterable) and not is_zero_dim_ndarray:
+                return '"[%s]"' % (', '.join(map(str, k)))
+            else:
+                return k
+
+        if not self.writer:
+            self.keys = sorted(logs.keys())
+
+            class CustomDialect(csv.excel):
+                delimiter = self.sep
+
+            self.writer = csv.DictWriter(self.csv_file,
+                                         fieldnames=['epoch', "time", "lr"] +
+                                         self.keys, dialect=CustomDialect)
+            if self.append_header:
+                self.writer.writeheader()
+        ##
+        if self.compute_auc:
+            if not batch_size:
+                numfiles = len(datagen_val_output.filenames)
+            steps = len(datagen_val_output.filenames)/prms['batch_size']
+            out = model.predict_generator(self.datagen_val_output, 
+                                          steps=steps)
+        time_ = time.time()
+        row_dict = OrderedDict(epoch=epoch,
+                               time = time_ - self._prev_time_,
+                               lr=self.lr,)
+        self._prev_time_ = time_
+        row_dict.update((key, handle_value(logs[key])) for key in self.keys)
+        self.writer.writerow(row_dict)
+        self.csv_file.flush()
+
+    @property
+    def lr(self): 
+        lr = float(K.get_value(self.model.optimizer.lr))
+        return lr
+
+
+
+    def on_train_end(self, logs=None):
+        self.csv_file.close()
+        self.writer = None
